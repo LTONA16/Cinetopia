4b3a8a865756b68c66e408fb46c8236652581d3e
LTONA16 <luisftona@gmail.com>
2026-01-21 23:36:55 -0700

    Add title, dimensions and fragments

--- app/src/main/java/com/example/cinetopia/MainActivity.kt
+++ app/src/main/java/com/example/cinetopia/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.example.cinetopia
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.cinetopia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.BottomNV.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Item_inicio -> {
+                    true
+                }
+                R.id.Item_cuenta -> {
+                    true
+                }
+                R.id.Item_boletos -> {
+                    true
+                }
+                R.id.Item_dulceria -> {
+                    true
+                }
+                R.id.Item_recompensas -> {
+                    true
+                }
+                else -> { false }
+            }
         }
     }
 }
