0c372ab22ecbf9cdf95cdd8fb9b2b8f013d3567d
LTONA16 <luisftona@gmail.com>
2026-01-28 20:28:04 -0700

    Add Email_login activity

--- app/src/main/java/com/example/cinetopia/Opciones_Login/Login_email.kt
+++ app/src/main/java/com/example/cinetopia/Opciones_Login/Login_email.kt
@@ -1,33 +1,21 @@
-package com.example.cinetopia
+package com.example.cinetopia.Opciones_Login
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.cinetopia.Opciones_Login.Login_email
+import com.example.cinetopia.R
 
-class OpcionesLogin : AppCompatActivity() {
+class Login_email : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_opciones_login)
+        setContentView(R.layout.activity_login_email)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // --- AQUÍ EMPIEZA TU LÓGICA DE REDIRECCIÓN ---
-
-        val btnIngresarEmail = findViewById<Button>(R.id.Ingresar_email)
-
-        btnIngresarEmail.setOnClickListener {
-            val intent = Intent(this, Login_email::class.java)
-            startActivity(intent)
-        }
-
     }
 }
